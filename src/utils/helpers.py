058e33d593306c52d05384a0cfdee8eb0c8eb96b
dKosarevsky <if@kosarevsky.ru>
2024-04-04 02:26:24 +0300

    feat(app): add helpers

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -2,16 +2,17 @@ import argparse
 import os
 from pathlib import Path
 from string import Template
+from typing import Any
 
 import cv2
 import numpy as np
 import streamlit as st
-from streamlit.components.v1 import html
 import yaml
+from streamlit.components.v1 import html
 
 
 @st.cache_data
-def get_arguments():
+def get_arguments() -> tuple[str, int, str]:
     """Return the values of CLI params"""
     parser = argparse.ArgumentParser()
     parser.add_argument("--image_folder", default="images")
@@ -23,26 +24,28 @@ def get_arguments():
 
 
 @st.cache_data
-def get_images_list(path_to_folder: str) -> list:
+def get_images_list(path_to_folder: str) -> list[str]:
     """Return the list of images from folder
     Args:
         path_to_folder (str): absolute or relative path to the folder with images
     """
-    image_names_list = [x for x in os.listdir(path_to_folder) if x[-3:] in ["jpg", "peg", "png"]]
-    return image_names_list
+    return [x for x in os.listdir(path_to_folder) if x[-3:] in ["jpg", "peg", "png"]]
 
 
 @st.cache_data
-def load_image(image_name: str, path_to_folder: str, bgr2rgb: bool = True) -> np.ndarray:
+def load_image(
+    image_name: str,
+    path_to_folder: str,
+    bgr2rgb: bool = True,
+) -> np.ndarray:
     """Load the image
     Args:
         image_name (str): name of the image
         path_to_folder (str): path to the folder with image
         bgr2rgb (bool): converts BGR image to RGB if True
-        read_mode (int): OpenCV read image format. Default: IMREAD_COLOR
     """
-    path_to_image = os.path.join(path_to_folder, image_name)
-    image = cv2.imread(path_to_image)
+    path_to_image = Path(path_to_folder) / image_name
+    image = cv2.imread(str(path_to_image))
     if bgr2rgb:
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     return image
@@ -53,29 +56,38 @@ def upload_image(bgr2rgb: bool = True) -> np.ndarray:
     Args:
         bgr2rgb (bool): converts BGR image to RGB if True
     """
-    file = st.sidebar.file_uploader("Upload your image (jpg, jpeg, or png)", ["jpg", "jpeg", "png"])
-    image = cv2.imdecode(np.fromstring(file.read(), np.uint8), 1)  # type: ignore
+    file = st.sidebar.file_uploader(
+        "Upload your image (jpg, jpeg, or png)",
+        ["jpg", "jpeg", "png"],
+    )
+    image = cv2.imdecode(np.fromstring(file.read(), np.uint8), 1)
     if bgr2rgb:
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     return image
 
 
 @st.cache_data
-def load_augmentations_config(placeholder_params: dict, path_to_config: str = "configs/augmentations.yml") -> dict:
+def load_augmentations_config(
+    placeholder_params: dict[str, list[dict[str, list[int] | str | int]]],
+    path_to_config: str = "configs/augmentations.yml",
+) -> dict[str, list[dict[str, list[int] | str | int]]]:
     """Load the yaml config with params of all transforms
     Args:
         placeholder_params (dict): dict with values of placeholders
         path_to_config (str): path to the yaml config file
     """
-    with open(path_to_config) as config_file:
+    with open(path_to_config) as config_file:  # noqa: PTH123
         augmentations = yaml.safe_load(config_file)
-    for _, params in augmentations.items():
+    for params in augmentations.values():
         [fill_placeholders(param, placeholder_params) for param in params]
     return augmentations
 
 
 @st.cache_data
-def load_augmentations_configs_from_folder(placeholder_params: dict, directory: str = "configs") -> dict:
+def load_augmentations_configs_from_folder(
+    placeholder_params: dict[str, list[dict[str, list[int] | str | int]]],
+    directory: str = "configs",
+) -> dict[str, list[dict[str, list[int] | str | int]]]:
     """Load the yaml config with params of all transforms
     Args:
         placeholder_params (dict): dict with values of placeholders
@@ -87,7 +99,10 @@ def load_augmentations_configs_from_folder(placeholder_params: dict, directory: 
     return result
 
 
-def fill_placeholders(params: dict, placeholder_params: dict) -> dict:
+def fill_placeholders(
+    params: dict[str, Any],
+    placeholder_params: dict[str, list[dict[str, list[int] | str | int]]],
+) -> dict[str, list[dict[str, list[int] | str | int]]]:
     """Fill the placeholder values in the config file
     Args:
         params (dict): original params dict with placeholders
@@ -103,25 +118,23 @@ def fill_placeholders(params: dict, placeholder_params: dict) -> dict:
                         params[k].append(placeholder_params[element])
                     else:
                         params[k].append(element)
+            elif v in placeholder_params:
+                params[k] = placeholder_params[v]
             else:
-                if v in placeholder_params:
-                    params[k] = placeholder_params[v]
-                else:
-                    params[k] = v
+                params[k] = v
         params.pop("placeholder")
     return params
 
 
-def get_params_string(param_values: dict) -> str:
+def get_params_string(param_values: dict[str, str | int]) -> str:
     """Generate the string from the dict with parameters
     Args:
         param_values (dict): dict of "param_name" -> "param_value"
     """
-    params_string = ", ".join([k + "=" + str(param_values[k]) for k in param_values.keys()])
-    return params_string
+    return ", ".join([k + "=" + str(param_values[k]) for k in param_values])
 
 
-def get_placeholder_params(image):
+def get_placeholder_params(image: np.ndarray) -> dict[str, int]:
     return {
         "image_width": image.shape[1],
         "image_height": image.shape[0],
@@ -130,24 +143,37 @@ def get_placeholder_params(image):
     }
 
 
-def select_transformations(augmentations: dict, interface_type: str) -> list:
+def select_transformations(
+    augmentations: dict[str, list[dict[str, list[int] | str | int]]],
+    interface_type: str,
+) -> list[str]:
     # in the Simple mode you can choose only one transform
     if interface_type == "Simple":
-        return [st.sidebar.selectbox("Select a transformation:", sorted(list(augmentations.keys())))]
+        return [
+            st.sidebar.selectbox(
+                "Select a transformation:",
+                sorted(augmentations.keys()),
+            ),
+        ]
     # in the professional mode you can choose several transforms
     # interface_type == "Professional":
-    transform_names = [st.sidebar.selectbox("Select transformation №1:", sorted(list(augmentations.keys())))]
+    transform_names = [
+        st.sidebar.selectbox(
+            "Select transformation №1:",
+            sorted(augmentations.keys()),
+        ),
+    ]
     while transform_names[-1] != "None":
         transform_names.append(
             st.sidebar.selectbox(
                 f"Select transformation №{len(transform_names) + 1}:",
-                ["None"] + sorted(list(augmentations.keys())),
-            )
+                ["None", *sorted(augmentations.keys())],
+            ),
         )
     return transform_names[:-1]
 
 
-def show_random_params(data: dict, interface_type: str = "Professional") -> None:
+def show_random_params(data: dict[str, Any], interface_type: str = "Professional") -> None:
     """Shows random params used for transformation (from A.ReplayCompose)"""
     if interface_type == "Professional":
         st.subheader("Random params used")
@@ -157,8 +183,8 @@ def show_random_params(data: dict, interface_type: str = "Professional") -> None
         st.write(random_values)
 
 
-def render_ga_code(tracking_id):
-    GA_CODE = Template(
+def render_ga_code(tracking_id: str) -> html:
+    ga_code = Template(
         """
         <!-- Google tag (gtag.js) -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=$tracking_id"></script>
@@ -168,6 +194,6 @@ def render_ga_code(tracking_id):
           gtag('js', new Date());
           gtag('config', '$tracking_id');
         </script>
-        """
+        """,
     )
-    return html(GA_CODE.substitute(tracking_id=tracking_id), height=0, width=0)
+    return html(ga_code.substitute(tracking_id=tracking_id), height=0, width=0)
